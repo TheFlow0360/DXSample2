76924445cb6425b37f06442fa4801d560477bccc
Florian Koch <fkoch@selectline.de>
2016-08-25 10:51:57 +0200

    Added project

---
+++ CustomGrid.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using DevExpress.Xpf.Grid;
+
+namespace DXSample2
+{
+    public class CustomGrid : GridControl
+    {
+        public static readonly DependencyProperty GroupingAndSummariesProperty = DependencyProperty.Register(
+            "GroupingAndSummaries",
+            typeof(Boolean),
+            typeof(CustomGrid),
+            new PropertyMetadata(null));
+
+        public Boolean GroupingAndSummaries
+        {
+            get { return (Boolean)this.GetValue(GroupingAndSummariesProperty); }
+            set
+            {
+                this.SetValue(GroupingAndSummariesProperty, value);
+                ((TableView)View).ShowGroupPanel = value;
+                ((TableView)View).AllowGrouping = value;
+                ((TableView)View).ShowTotalSummary = value;
+            }
+        }
+    }
+}
